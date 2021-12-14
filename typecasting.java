fc3b5ef44361a29fb0c2b6701e5601cdb03c5fcf
Praduman2k19 <praduman2k19@gmail.com>
2021-12-14 23:11:17 +0530

    .

--- typecasting.java
+++ typecasting.java
@@ -1,7 +1,21 @@
 public class typecasting {
     public static void main(String[] args) {
         
-        System.out.print("jhgfds");
+        // Widening Casting (automatically) - converting a smaller type to a larger type size
+        // byte -> short -> char -> int -> long -> float -> double
+
+        // Narrowing Casting (manually) - converting a larger type to a smaller size type
+        // double -> float -> long -> int -> char -> short -> byte
+
+        int a=5;
+        double b=(double)a;
+        System.out.println(a);
+        System.out.println(b);
+
+        double c=5.6;
+        int d=(int) c;
+        System.out.println(c);
+        System.out.println(d);
     }
     
 }
